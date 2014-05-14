ad04c1e90bc0f0d98a3cbc86b6e0f27826dd0e55
Sebastian-tbf <Sebastian@tbf.mx>
2014-05-14 12:35:04 -0500

    ajustes paypal

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -14,9 +14,11 @@ class Cart < ActiveRecord::Base
 	def remove_product(product_id)
 		current_item = line_items.find_by(product_id: product_id)
 		if current_item
-			current_item.quantity += -1
-		else
-			current_item = line_items.build(product_id: product_id)
+			if current_item.quantity >= 0
+				current_item.quantity += -1
+			end
+		#else
+		#	current_item = line_items.build(product_id: product_id)
 		end
 		current_item
 	end
